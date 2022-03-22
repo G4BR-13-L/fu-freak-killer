3dd5a6956a9993c7c843f6192377ed6d009a8dab
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-03-21 22:47:03 -0300

    Vamo que vamo fazendo esse TDD

--- exs/EX04/Tests/TurmaTest.java
+++ exs/EX04/Tests/TurmaTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 
 import src.Turma;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,14 +23,19 @@ import org.junit.jupiter.api.Nested;
 public class TurmaTest {
     
     Turma turma;
+
     @BeforeEach
     public void iniciarTeste(){
-        turma = new Turma();
+        int[] codigoDaTurma = { 1,2,3 };
+        turma = new Turma(codigoDaTurma);
     }
 
     @Test
-    public void verificaQuantidadeDeAlunos(){
-        assertEquals(20, turma.getQuantidadeAlunos());
+    public void verificaCodigoDaTurma(){
+        int[] codigoDaTurma = { 9,7,4 };
+        turma.setCodigoDaTurma(codigoDaTurma);
+        int[] arrayEsperado = { 0,0,0 };
+        assertArrayEquals({0,0,0}, turma.getCodigoDaTurma());
     }
 
     
